687883c21e36ebd1980921df5dd4c82490cc20ef
2in2in <88322988+2in2in@users.noreply.github.com>
2022-05-04 17:56:51 +0000

    In the middle of chapter9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_171904) do
+ActiveRecord::Schema.define(version: 2022_05_04_153650) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.string "image_id"
+    t.text "caption"
+    t.string "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
